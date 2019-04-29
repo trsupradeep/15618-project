de714a5449eeb5a31f90af72fc3b5222cf6cc34a
Supradeep T R <tr.supradeep@gmail.com>
2019-04-29 11:40:11 -0400

    Rust: Add seq, par code for matrix multiply
    Matrix multiplication code is completed with the serial and parallel code.
    
    Problem: Serial code is much slower than C++, need to check why.

--- rust/matrixmultiply/src/main.rs
+++ rust/matrixmultiply/src/main.rs
@@ -9,6 +9,9 @@ use rand::{thread_rng, Rng};
 use rayon::prelude::*;
 use std::time::{Duration, Instant};
 
+const MULT_CHUNK: usize = 1 * 1024;
+const LINEAR_CHUNK: usize = 64 * 1024;
+
 macro_rules! RM {
     ($row:expr, $col:expr, $W:expr) => {
         ($row * $W + $col) as usize
@@ -18,25 +21,17 @@ macro_rules! RM {
 fn main() {
     let matmul_config = parse_arguments();
 
-    let mut rng = rand::thread_rng();
+    let mut rng = thread_rng();
 
     let num_mat_elements = matmul_config.size * matmul_config.size;
 
-    // let A: Vec<i32> = (0..num_mat_elements)
-    //     .map(|_| rng.gen_range(-10, 10))
-    //     .collect();
+    let mut m_a: Vec<i32> = (0..num_mat_elements)
+        .map(|_| rng.gen_range(-10, 10))
+        .collect();
 
-    let mut A: Vec<i32> = vec![1; num_mat_elements as usize];
-    A[1] = 2;
-    A[2] = 3;
-    A[3] = 4;
-
-    let mut B = vec![1; num_mat_elements as usize];
-    B[1] = 2;
-    B[2] = 3;
-    B[3] = 4;
-
-    let mut C: Vec<i32> = vec![0; num_mat_elements as usize];
+    let mut m_b: Vec<i32> = (0..num_mat_elements)
+        .map(|_| rng.gen_range(-10, 10))
+        .collect();
 
     // Set the number of threads for rayon
     rayon::ThreadPoolBuilder::new()
@@ -44,7 +39,7 @@ fn main() {
         .build_global()
         .unwrap();
 
-    do_runs(&matmul_config, &mut A, &mut B, &mut C);
+    do_runs(&matmul_config, &mut m_a, &mut m_b);
 }
 
 // Configuration file, reflects command line options
@@ -56,28 +51,26 @@ pub struct MatMulConfig {
     pub code_config: u32,
 }
 
-pub fn do_runs(matmul_config: &MatMulConfig, A: &mut [i32], B: &mut [i32], C: &mut [i32]) {
+pub fn do_runs(matmul_config: &MatMulConfig, m_a: &mut [i32], m_b: &mut [i32]) {
     let num_runs = matmul_config.num_of_runs;
 
     let mut serial_time = Duration::new(100, 0);
-    let mut stable_par_time = Duration::new(100, 0);
     let mut row_parallel_time = Duration::new(100, 0);
-    // let mut crossbeam_parallel_time = Duration::new(100, 0);
+    let mut quad_parallel_time = Duration::new(100, 0);
+
+    let num_mat_elements = matmul_config.size * matmul_config.size;
+
+    let mut serial_m_c: Vec<i32> = vec![0; num_mat_elements as usize];
 
     if (matmul_config.code_config == 0) || (matmul_config.code_config == 2) {
         for _ in 0..num_runs {
             let serial_start = Instant::now();
-            matmul_serial(matmul_config, A, B, C);
+            // matmul_serial(matmul_config.size as usize, m_a, m_b, &mut serial_m_c[..]);
+            matmul_seq(m_a, m_b, &mut serial_m_c[..]);
             let serial_end = Instant::now();
 
             serial_time = std::cmp::min(serial_time, serial_end.duration_since(serial_start));
         }
-
-        // println!("A: {:?}", A);
-        // println!("B: {:?}", B);
-        // println!("C: {:?}", C);
-
-        // Check correctness
 
         println!(
             "[matmul-rust serial]: \t[{:?}] ms",
@@ -85,80 +78,58 @@ pub fn do_runs(matmul_config: &MatMulConfig, A: &mut [i32], B: &mut [i32], C: &m
         );
     }
 
-    if (matmul_config.code_config == 0) || (matmul_config.code_config == 2) {
+    if (matmul_config.code_config == 0) || (matmul_config.code_config == 1) {
+        let mut par_row_m_c: Vec<i32> = vec![0; num_mat_elements as usize];
+
         for _ in 0..num_runs {
             let par_start = Instant::now();
-            matmul_par(matmul_config, A, B, C);
+            matmul_par_row(matmul_config.size as usize, m_a, m_b, &mut par_row_m_c[..]);
             let par_end = Instant::now();
 
             row_parallel_time = std::cmp::min(row_parallel_time, par_end.duration_since(par_start));
         }
 
         println!(
-            "[matmul-rust serial]: \t[{:?}] ms",
+            "[matmul-rust par_row]: \t[{:?}] ms",
             row_parallel_time.as_micros() as f64 / 1000 as f64
         );
 
         if matmul_config.code_config == 0 {
+            assert_eq!(par_row_m_c, serial_m_c);
             println!(
                 "++++ \t\t({:.2}x speedup from {:?} threads)\n",
                 serial_time.as_micros() as f64 / row_parallel_time.as_micros() as f64,
                 matmul_config.num_threads
             );
         }
+
+        /////////////////////////////////////////////
+        // let mut par_quad_m_c: Vec<i32> = vec![0; num_mat_elements as usize];
+
+        // for _ in 0..num_runs {
+        //     let par_start = Instant::now();
+        //     matmulz(m_a, m_b, &mut par_quad_m_c[..]);
+        //     let par_end = Instant::now();
+
+        //     quad_parallel_time =
+        //         std::cmp::min(quad_parallel_time, par_end.duration_since(par_start));
+        // }
+
+        // assert_eq!(par_quad_m_c, serial_m_c);
+
+        // println!(
+        //     "[matmul-rust par_row]: \t[{:?}] ms",
+        //     quad_parallel_time.as_micros() as f64 / 1000 as f64
+        // );
+
+        // if matmul_config.code_config == 0 {
+        //     println!(
+        //         "++++ \t\t({:.2}x speedup from {:?} threads)\n",
+        //         serial_time.as_micros() as f64 / quad_parallel_time.as_micros() as f64,
+        //         matmul_config.num_threads
+        //     );
+        // }
     }
-
-    //     //////////////////////
-    //     // Row parallel Test
-
-    //     for _ in 0..num_runs {
-    //         let rayon_row_start = Instant::now();
-    //         rayon_mandelbrot_row(&mandel_config, image);
-    //         let rayon_row_end = Instant::now();
-
-    //         row_parallel_time = std::cmp::min(
-    //             row_parallel_time,
-    //             rayon_row_end.duration_since(rayon_row_start),
-    //         );
-    //     }
-
-    //     println!(
-    //         "[mandelbrot-rust row]: \t\t\t[{:?}] ms",
-    //         row_parallel_time.as_micros() as f64 / 1000 as f64
-    //     );
-
-    //     if mandel_config.code_config == 0 {
-    //         println!(
-    //             "++++ \t\t({:.2}x speedup from {:?} threads) \n",
-    //             serial_time.as_micros() as f64 / row_parallel_time.as_micros() as f64,
-    //             mandel_config.num_threads
-    //         );
-    //     }
-
-    //     for _ in 0..num_runs {
-    //         let crossbeam_row_start = Instant::now();
-    //         crossbeam_manderlbrot_row(&mandel_config, image);
-    //         let crossbeam_row_end = Instant::now();
-
-    //         crossbeam_parallel_time = std::cmp::min(
-    //             crossbeam_parallel_time,
-    //             crossbeam_row_end.duration_since(crossbeam_row_start),
-    //         );
-    //     }
-
-    //     println!(
-    //         "[mandelbrot-rust crossbeam row]: \t[{:?}] ms",
-    //         crossbeam_parallel_time.as_micros() as f64 / 1000 as f64
-    //     );
-
-    //     if mandel_config.code_config == 0 {
-    //         println!(
-    //             "++++ \t\t({:.2}x speedup from {:?} threads)\n",
-    //             serial_time.as_micros() as f64 / crossbeam_parallel_time.as_micros() as f64,
-    //             mandel_config.num_threads
-    //         );
-    //     }
-    // }
 }
 
 pub fn parse_arguments() -> MatMulConfig {
@@ -227,39 +198,178 @@ pub fn parse_arguments() -> MatMulConfig {
     }
 }
 
-pub fn matmul_serial(matmul_config: &MatMulConfig, A: &[i32], B: &[i32], C: &mut [i32]) {
-    let size = matmul_config.size;
-    let height = size;
-    let width = size;
-    for i in 0..height {
-        for j in 0..width {
-            for k in 0..size {
-                C[RM!(i, j, size)] += A[RM!(i, k, size)] * B[RM!(k, j, size)];
+pub fn matmul_serial(size: usize, m_a: &[i32], m_b: &[i32], m_c: &mut [i32]) {
+    let iter_c = m_c.chunks_mut(size);
+
+    iter_c.enumerate().for_each(|(c_idx, c_slice)| {
+        let a_slice = &m_a[RM!(c_idx, 0, size)..RM!(c_idx + 1, 0, size)];
+        c_slice.iter_mut().enumerate().for_each(|(col, c)| {
+            *c = a_slice
+                .iter()
+                .enumerate()
+                .map(|(index, addr)| *addr * m_b[RM!(index, col as usize, size)])
+                .sum()
+        });
+    });
+}
+
+pub fn matmul_par_row(size: usize, m_a: &[i32], m_b: &[i32], m_c: &mut [i32]) {
+    let iter_c = m_c.par_chunks_mut(size);
+
+    iter_c.enumerate().for_each(|(c_idx, c_slice)| {
+        let a_slice = &m_a[RM!(c_idx, 0, size)..RM!(c_idx + 1, 0, size)];
+        c_slice.iter_mut().enumerate().for_each(|(col, c)| {
+            *c = a_slice
+                .iter()
+                .enumerate()
+                .map(|(index, addr)| *addr * m_b[RM!(index, col as usize, size)])
+                .sum()
+        });
+    });
+}
+
+pub fn matmul_seq(m_a: &[i32], m_b: &[i32], dest: &mut [i32]) {
+    // Multiply in row-major order.
+    // D[i,j] = sum for all k A[i,k] * B[k,j]
+    let bits = dest.len().trailing_zeros() / 2;
+    let n = 1 << bits;
+    for i in 0..n {
+        for j in 0..n {
+            let mut sum = 0;
+            for k in 0..n {
+                sum +=
+                    unsafe { m_a.get_unchecked(i << bits | k) * m_b.get_unchecked(k << bits | j) };
             }
+            dest[i << bits | j] = sum;
         }
     }
 }
 
-pub fn matmul_par(matmul_config: &MatMulConfig, A: &mut [i32], B: &mut [i32], C: &mut [i32]) {
-    let size = matmul_config.size;
-    let height = size;
-    let width = size;
-    println!("Parallel");
+// Multiply two square power of two matrices, given in Z-order.
+pub fn matmulz(a: &[i32], b: &[i32], dest: &mut [i32]) {
+    if a.len() <= MULT_CHUNK {
+        let bits = dest.len().trailing_zeros() / 2;
+        let size = 1 << bits;
+        matmul_serial(size, a, b, dest);
+        return;
+    }
 
-    let iter_a = A.par_chunks_mut(matmul_config.size as usize);
-    let iter_c = C.par_chunks_mut(matmul_config.size as usize);
+    // Allocate uninitialized scratch space.
+    let mut tmp = vec![0; dest.len()];
 
-    iter_a
-        .zip(iter_c)
-        .enumerate()
-        .for_each(|(_, (a_slice, c_slice))| {
-            let size = matmul_config.size as usize;
-            for col in 0..size {
-                let mut c = 0 as i32;
-                a_slice.iter().enumerate().for_each(|(index, addr)| {
-                    c += *addr * B[RM!(index, col as usize, size as usize)];
-                });
-                c_slice[col as usize] = c;
-            }
-        });
+    let (a1, a2, a3, a4) = quarter_chunks(a);
+    let (b1, b2, b3, b4) = quarter_chunks(b);
+    {
+        let (d1, d2, d3, d4) = quarter_chunks_mut(dest);
+        let (t1, t2, t3, t4) = quarter_chunks_mut(&mut tmp[..]);
+        // Multiply 8 submatrices
+        join8(
+            || matmulz(a1, b1, d1),
+            || matmulz(a1, b2, d2),
+            || matmulz(a3, b1, d3),
+            || matmulz(a3, b2, d4),
+            || matmulz(a2, b3, t1),
+            || matmulz(a2, b4, t2),
+            || matmulz(a4, b3, t3),
+            || matmulz(a4, b4, t4),
+        );
+    }
+
+    // Sum each quarter
+    rmatsum(tmp.as_mut(), dest);
+}
+
+fn quarter_chunks<'a>(v: &'a [i32]) -> (&'a [i32], &'a [i32], &'a [i32], &'a [i32]) {
+    let mid = v.len() / 2;
+    let quarter = mid / 2;
+    let (left, right) = v.split_at(mid);
+    let (a, b) = left.split_at(quarter);
+    let (c, d) = right.split_at(quarter);
+    (a, b, c, d)
+}
+
+fn quarter_chunks_mut<'a>(
+    v: &'a mut [i32],
+) -> (&'a mut [i32], &'a mut [i32], &'a mut [i32], &'a mut [i32]) {
+    let mid = v.len() / 2;
+    let quarter = mid / 2;
+    let (left, right) = v.split_at_mut(mid);
+    let (a, b) = left.split_at_mut(quarter);
+    let (c, d) = right.split_at_mut(quarter);
+    (a, b, c, d)
+}
+
+// Any layout works, we're just adding by element.
+fn rmatsum(src: &[i32], dest: &mut [i32]) {
+    dest.par_iter_mut()
+        .zip(src.par_iter())
+        .for_each(|(d, s)| *d += *s);
+}
+
+fn rmatsub(src: &[i32], dest: &mut [i32]) {
+    dest.par_iter_mut()
+        .zip(src.par_iter())
+        .for_each(|(d, s)| *d -= *s);
+}
+
+fn rcopy(src: &[i32], dest: &mut [i32]) {
+    if dest.len() <= LINEAR_CHUNK {
+        dest.copy_from_slice(src);
+        return;
+    }
+
+    let mid = dest.len() / 2;
+    let (s1, s2) = src.split_at(mid);
+    let (d1, d2) = dest.split_at_mut(mid);
+    rayon::join(|| rcopy(s1, d1), || rcopy(s2, d2));
+}
+
+fn join4<F1, F2, F3, F4, R1, R2, R3, R4>(f1: F1, f2: F2, f3: F3, f4: F4) -> (R1, R2, R3, R4)
+where
+    F1: FnOnce() -> R1 + Send,
+    R1: Send,
+    F2: FnOnce() -> R2 + Send,
+    R2: Send,
+    F3: FnOnce() -> R3 + Send,
+    R3: Send,
+    F4: FnOnce() -> R4 + Send,
+    R4: Send,
+{
+    let ((r1, r2), (r3, r4)) = rayon::join(|| rayon::join(f1, f2), || rayon::join(f3, f4));
+    (r1, r2, r3, r4)
+}
+
+fn join8<F1, F2, F3, F4, F5, F6, F7, F8, R1, R2, R3, R4, R5, R6, R7, R8>(
+    f1: F1,
+    f2: F2,
+    f3: F3,
+    f4: F4,
+    f5: F5,
+    f6: F6,
+    f7: F7,
+    f8: F8,
+) -> (R1, R2, R3, R4, R5, R6, R7, R8)
+where
+    F1: FnOnce() -> R1 + Send,
+    R1: Send,
+    F2: FnOnce() -> R2 + Send,
+    R2: Send,
+    F3: FnOnce() -> R3 + Send,
+    R3: Send,
+    F4: FnOnce() -> R4 + Send,
+    R4: Send,
+    F5: FnOnce() -> R5 + Send,
+    R5: Send,
+    F6: FnOnce() -> R6 + Send,
+    R6: Send,
+    F7: FnOnce() -> R7 + Send,
+    R7: Send,
+    F8: FnOnce() -> R8 + Send,
+    R8: Send,
+{
+    let (((r1, r2), (r3, r4)), ((r5, r6), (r7, r8))) = rayon::join(
+        || rayon::join(|| rayon::join(f1, f2), || rayon::join(f3, f4)),
+        || rayon::join(|| rayon::join(f5, f6), || rayon::join(f7, f8)),
+    );
+    (r1, r2, r3, r4, r5, r6, r7, r8)
 }
