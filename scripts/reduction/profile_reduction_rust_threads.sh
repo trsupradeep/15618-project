6f4a6dfab62e4c7d32f3a8d9d6777fc548c2f51d
Supradeep T R <tr.supradeep@gmail.com>
2019-05-01 00:57:31 -0400

    Scripts: Fix reduction type flag
    Fixed reduction type flag in both the scripts for reduction.

--- scripts/reduction/profile_reduction_rust_threads.sh
+++ scripts/reduction/profile_reduction_rust_threads.sh
@@ -18,16 +18,18 @@ cc="-c 1"
 
 size=(100000000 500000000 1000000000 2000000000)
 
+reduc=("-p 0" "-p 1")
+
 echo "--------------------------------------------------------------------------"
 echo " Performing serial runs"
 echo "--------------------------------------------------------------------------"
 for s in 0 1 2 3
 do
-  for reduc in 0 1
+  for re in 0 1
   do
     flag="-s ${size[${s}]} -r 3 ${reduc[re]}"
-    echo "${exec_path}/${exec} ${flag} ${cc} > ${save_path}/thread_0_${size[${s}]}.log"
-    ${exec_path}/${exec} ${flag} ${cc} > ${save_path}/thread_0_${size[${s}]}.log
+    echo "${exec_path}/${exec} ${flag} ${cc} > ${save_path}/thread_0_${size[${s}]}_${re}.log"
+    ${exec_path}/${exec} ${flag} ${cc} > ${save_path}/thread_0_${size[${s}]}_${re}.log
   done
 done
 
@@ -44,11 +46,11 @@ do
 
   for s in 0 1 2 3
   do
-    for reduc in 0 1
+    for re in 0 1
     do
       flag="-n ${size[${s}]} -r 3 -t ${t} ${reduc[re]]}"
-      echo "${exec_path}/${exec} ${flag} ${cc} > ${save_path}/thread_${t}_${size[${s}]}.log"
-      ${exec_path}/${exec} ${flag} ${cc} > ${save_path}/thread_${t}_${size[${s}]}.log
+      echo "${exec_path}/${exec} ${flag} ${cc} > ${save_path}/thread_${t}_${size[${s}]}_${re}.log"
+      ${exec_path}/${exec} ${flag} ${cc} > ${save_path}/thread_${t}_${size[${s}]}_${re}.log
     done
   done
 
